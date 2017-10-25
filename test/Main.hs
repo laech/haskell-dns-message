8557a62ea9f0d5d9782de91e01f6bac8671f3f7f
Lae <laec@protonmail.ch>
2017-10-25 21:12:24 +1300

    Fix put error.

--- test/Main.hs
+++ test/Main.hs
@@ -1,70 +1,53 @@
+import           Control.Exception
 import           Data.Binary.Get
 import           Data.Binary.Put
 import           Data.ByteString       as B
 import           Data.ByteString.Char8 as C
 import           Data.ByteString.Lazy  as L
 import           Network.DNS.Message
+import           Numeric
 import           Test.HUnit
 
-testDecode :: FilePath -> Message -> Test
-testDecode path expected =
-  TestCase $ do
-    input <- L.readFile path
-    let result = runGetOrFail getMessage input
-    case result of
-      Left (_, _, info)    -> assertFailure (show info)
-      Right (_, _, actual) -> assertEqual "" expected actual
+testFile :: FilePath -> Message -> Test
+testFile path expected =
+  TestList
+  [ testDecode path expected
+  , testPutGet path expected
+  ]
 
-testPutThenGet :: Message -> Test
-testPutThenGet expected = TestLabel ("put . get = id for " ++ show expected) $ TestCase $ assertEqual "" expected (runGet getMessage $ runPut $ putMessage expected)
+testDecode :: FilePath -> Message -> Test
+testDecode path expected = TestLabel "testDecode" $ TestCase $ do
+  input <- L.readFile path
+  let result = runGetOrFail getMessage input
+  case result of
+    Left (_, _, info)    -> assertFailure (show info)
+    Right (_, _, actual) -> assertEqual "" expected actual
+
+testPutGet :: FilePath -> Message -> Test
+testPutGet path expected = TestLabel "testPutGet" $ TestCase $ do
+  expectedBytes <- L.readFile path
+  let actualBytes = runPut $ putMessage expected
+  test actualBytes `catch` handle expectedBytes actualBytes
+  where
+
+    test actualBytes = do
+      let actual = runGet getMessage actualBytes
+      assertEqual "" expected actual
+
+    handle :: L.ByteString -> L.ByteString -> SomeException -> Assertion
+    handle expectedBytes actualBytes e = fail $ show e ++
+      "\n\n      Expected:\n" ++ show expected ++
+      "\n\nExpected bytes:\n" ++ printHex expectedBytes ++
+      "\n\n  Actual bytes:\n" ++ printHex actualBytes
+
+printHex :: L.ByteString -> String
+printHex = L.foldr (\w result -> showHex w (' ' : result)) ""
 
 main =
   runTestTT $
   TestList
-    [ testPutThenGet
-      Message
-      { identifier = 1
-      , flags = 101
-      , questions = []
-      , answers = []
-      , authorities = []
-      , additionals = []
-      }
-
-    , testPutThenGet
-      Message
-      { identifier = 11
-      , flags = 1010
-      , questions =
-        [ Question
-          { qname = Domain []
-          , qtype = typeA
-          , qclass = classIN
-          }
-        ]
-      , answers = []
-      , authorities = []
-      , additionals = []
-      }
-
-    , testPutThenGet
-      Message
-      { identifier = 11
-      , flags = 1010
-      , questions =
-        [ Question
-          { qname = Domain [mklabel "google", mklabel "com"]
-          , qtype = typeA
-          , qclass = classIN
-          }
-        ]
-      , answers = []
-      , authorities = []
-      , additionals = []
-      }
-
-    , testDecodeQueryTypeA
-    , testDecodeResponseTypeA
+    [ testAQuery
+    , testAResponse
     ]
 
 encodeIPv4 i1 i2 i3 i4 = foldMap B.singleton [i1, i2, i3, i4]
@@ -74,62 +57,58 @@ encodeIPv6 i1 i2 i3 i4 i5 i6 i7 i8 i9 i10 i11 i12 i13 i14 i15 i16 =
     B.singleton
     [i1, i2, i3, i4, i5, i6, i7, i8, i9, i10, i11, i12, i13, i14, i15, i16]
 
-testDecodeQueryTypeA =
-  testDecode
-    "test-res/A.query"
-    Message
-    { identifier = 0x7d3c
-    , flags = 0x0120
-    , questions =
-        [ Question
-          { qname = Domain [mklabel "google", mklabel "com"]
-          , qtype = typeA
-          , qclass = classIN
-          }
-        ]
-    , answers = []
-    , authorities = []
-    , additionals =
-        [ ResourceRecord
-          { rname = Domain []
-          , rtype = typeOPT
-          , rclass = 4096
-          , rttl = 0
-          , rdata = B.empty
-          }
-        ]
-    }
+testAQuery = testFile "test-res/A.query"
+  Message
+  { identifier = 0x7d3c
+  , flags = 0x0120
+  , questions =
+      [ Question
+        { qname = Domain [mklabel "google", mklabel "com"]
+        , qtype = typeA
+        , qclass = classIN
+        }
+      ]
+  , answers = []
+  , authorities = []
+  , additionals =
+      [ ResourceRecord
+        { rname = Domain []
+        , rtype = typeOPT
+        , rclass = 4096
+        , rttl = 0
+        , rdata = B.empty
+        }
+      ]
+  }
 
-testDecodeResponseTypeA =
-  testDecode
-    "test-res/A.response"
-    Message
-    { identifier = 0x7d3c
-    , flags = 0x8180
-    , questions =
-        [ Question
-          { qname = Domain [mklabel "google", mklabel "com"]
-          , qtype = typeA
-          , qclass = classIN
-          }
-        ]
-    , answers =
-        [ ResourceRecord
-          { rname = Domain [mklabel "google", mklabel "com"]
-          , rtype = typeA
-          , rclass = classIN
-          , rttl = 299
-          , rdata = encodeIPv4 216 58 200 110
-          }
-        ]
-    , authorities = []
-    , additionals =
-        [ ResourceRecord
-          { rname = Domain []
-          , rtype = typeOPT
-          , rclass = 512
-          , rttl = 0
-          , rdata = B.empty
-          }
-        ]
-    }
+testAResponse = testFile "test-res/A.response"
+  Message
+  { identifier = 0x7d3c
+  , flags = 0x8180
+  , questions =
+      [ Question
+        { qname = Domain [mklabel "google", mklabel "com"]
+        , qtype = typeA
+        , qclass = classIN
+        }
+      ]
+  , answers =
+      [ ResourceRecord
+        { rname = Domain [mklabel "google", mklabel "com"]
+        , rtype = typeA
+        , rclass = classIN
+        , rttl = 299
+        , rdata = encodeIPv4 216 58 200 110
+        }
+      ]
+  , authorities = []
+  , additionals =
+      [ ResourceRecord
+        { rname = Domain []
+        , rtype = typeOPT
+        , rclass = 512
+        , rttl = 0
+        , rdata = B.empty
+        }
+      ]
+  }
